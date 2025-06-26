3e1e4dc7c1a5ae080db310eb4877ef6c2aa2c021
Simon Rodriguez <mrsimonrodriguez@gmail.com>
2025-06-26 17:43:27 +0200

    added a player spawn entity. if any asteroid collides with it, that frame+next gets invalidated for spawn. so we dont spawn inside a asteroid.

--- Assets/Game/Scripts/Game.cs
+++ Assets/Game/Scripts/Game.cs
@@ -15,6 +15,7 @@ namespace mygame
 
 		[Header("Prefabs Player")]
 		[SerializeField] GameObject _prefabPlayer;
+		[SerializeField] GameObject _prefabPlayerSpawn;
 		[SerializeField] GameObject _prefabMissile;
 
 		WorldBoundsManager _worldBoundsManager;
@@ -25,6 +26,8 @@ namespace mygame
 
 		int _score = 0;
 		int _lives = 0;
+
+		int _invalidSpawnFrame = 0;
 
 		void Awake()
 		{
@@ -40,16 +43,19 @@ namespace mygame
 			Assert.IsNotNull(_prefabAsteroidMedium, "Prefab object is not assigned. Please assign a prefab in the inspector.");
 			Assert.IsNotNull(_prefabAsteroidSmall, "Prefab object is not assigned. Please assign a prefab in the inspector.");
 			Assert.IsNotNull(_prefabPlayer, "Prefab object is not assigned. Please assign a prefab in the inspector.");
+			Assert.IsNotNull(_prefabPlayerSpawn, "Prefab object is not assigned. Please assign a prefab in the inspector.");
 			Assert.IsNotNull(_prefabMissile, "Prefab object is not assigned. Please assign a prefab in the inspector.");
 		}
 
 		void Start()
 		{
+			//Assuming all transforms are uniformed scaled
 			_halfSizeBigAsteroid = _prefabAsteroidBig.transform.localScale.x * .5f;
 			_halfSizeMediumAsteroid = _prefabAsteroidMedium.transform.localScale.x * .5f;
 			_halfSizeSmallAsteroid = _prefabAsteroidSmall.transform.localScale.x * .5f;
 			var halfSizeMissile = _prefabMissile.transform.localScale.x * .5f;
 			var halfSizePlayer = _prefabPlayer.transform.localScale.x * .5f;
+			var halfSizePlayerSpawn = _prefabPlayerSpawn.transform.localScale.x * .5f;
 
 			_lives = _tweaktable.PlayerLives;
 			EventsCenter.Invoke(new GameEvents.LivesChangedEvent(_lives)); //So UI can update with the dynamic value
@@ -59,6 +65,7 @@ namespace mygame
 			_entitiesManager.RegisterEntity(GameEntities.AsteroidSmall, _prefabAsteroidSmall, _halfSizeSmallAsteroid);
 			_entitiesManager.RegisterEntity(GameEntities.Missile, _prefabMissile, halfSizeMissile);
 			_entitiesManager.RegisterEntity(GameEntities.Player, _prefabPlayer, halfSizePlayer, ensureCapacity: 1); //Only want one of these
+			_entitiesManager.RegisterEntity(GameEntities.PlayerSpawn, _prefabPlayerSpawn, halfSizePlayerSpawn, ensureCapacity: 1); //Only want one of these
 
 			_entitiesManager.RegisterEntityLifetime(GameEntities.Missile, _tweaktable.MissilesSecondsToLive);
 
@@ -76,6 +83,12 @@ namespace mygame
 			_entitiesManager.RegisterCollisionSolver(GameEntities.Player, OnPlayerHit, GameEntities.AsteroidBig, OnNoop);
 			_entitiesManager.RegisterCollisionSolver(GameEntities.Player, OnPlayerHit, GameEntities.AsteroidMedium, OnNoop);
 			_entitiesManager.RegisterCollisionSolver(GameEntities.Player, OnPlayerHit, GameEntities.AsteroidSmall, OnNoop);
+
+			_entitiesManager.RegisterCollisionSolver(GameEntities.PlayerSpawn, OnPlayerSpawnHit, GameEntities.AsteroidBig, OnNoop);
+			_entitiesManager.RegisterCollisionSolver(GameEntities.PlayerSpawn, OnPlayerSpawnHit, GameEntities.AsteroidMedium, OnNoop);
+			_entitiesManager.RegisterCollisionSolver(GameEntities.PlayerSpawn, OnPlayerSpawnHit, GameEntities.AsteroidSmall, OnNoop);
+
+			_entitiesManager.Spawn(GameEntities.PlayerSpawn, Vector2.zero, Vector2.zero);
 
 			for (int i = 0; i < 3; i++)
 				_entitiesManager.Spawn(
@@ -95,6 +108,10 @@ namespace mygame
 		{
 			if (timeDelayUntilSpawn > 0f)
 				await Awaitable.WaitForSecondsAsync(timeDelayUntilSpawn);
+
+			//if anything is inside player spawn, we dont want to spawn
+			while (Time.frameCount <= _invalidSpawnFrame)
+				await Awaitable.NextFrameAsync();
 
 			SpawnPlayer();
 		}
@@ -122,6 +139,11 @@ namespace mygame
 		#endregion
 
 		#region COLLISION SOLVERS
+
+		void OnPlayerSpawnHit(EntityReference _, EntityReference __)
+		{
+			_invalidSpawnFrame = Time.frameCount + 1;
+		}
 
 		void OnMissileVsBigAsteroid(EntityReference missile, EntityReference asteroid)
 		{
--- Assets/Game/Scripts/GameEntities.cs
+++ Assets/Game/Scripts/GameEntities.cs
@@ -3,6 +3,7 @@ namespace mygame
 	public static class GameEntities
 	{
 		public const int Player = 0;
+		public const int PlayerSpawn = 5;
 		public const int Missile = 1;
 		public const int AsteroidBig = 2;
 		public const int AsteroidMedium = 3;
