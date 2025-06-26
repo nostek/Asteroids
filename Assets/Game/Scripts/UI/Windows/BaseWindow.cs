bd5ba383f217acf67152e123db3c86179a3f2ade
Simon Rodriguez <mrsimonrodriguez@gmail.com>
2025-06-26 22:25:08 +0200

    base window that has a close window function

---
+++ Assets/Game/Scripts/UI/Windows/BaseWindow.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityServiceLocator;
+
+namespace mygame
+{
+	public class BaseWindow : MonoBehaviour
+	{
+		protected WindowsManager _windowsManager;
+
+		protected virtual void Awake()
+		{
+			_windowsManager = ServiceLocator.Get<WindowsManager>();
+		}
+
+		protected void CloseWindow()
+		{
+			_windowsManager.CloseWindow(gameObject);
+		}
+	}
+}
