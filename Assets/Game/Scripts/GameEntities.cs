cbd6381b44cc257eec43dbbf05cebc537342dd39
Simon Rodriguez <mrsimonrodriguez@gmail.com>
2025-06-26 16:57:52 +0200

    use const ints for entity keys. would like to have used a enum, but dont want to cast to int when sending it in, or make EntityManager a generic.

---
+++ Assets/Game/Scripts/GameEntities.cs
@@ -0,0 +1,11 @@
+namespace mygame
+{
+	public static class GameEntities
+	{
+		public const int Player = 0;
+		public const int Missile = 1;
+		public const int AsteroidBig = 2;
+		public const int AsteroidMedium = 3;
+		public const int AsteroidSmall = 4;
+	}
+}
