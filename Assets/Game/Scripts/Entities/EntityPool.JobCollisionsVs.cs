365cd1d4a66d037043375c0b27809e7d7eb8298b
Simon Rodriguez <mrsimonrodriguez@gmail.com>
2025-06-27 10:26:24 +0200

    made two versions of the collision jobs. one checks for self intersection of the same entity and the other does not. goes against DRY but eliminates a compare in a tight loop

--- Assets/Game/Scripts/Entities/EntityPool.JobCollisionsVs.cs
+++ Assets/Game/Scripts/Entities/EntityPool.JobCollisionsVs.cs
@@ -16,7 +16,23 @@ namespace mygame
 				return new JobHandle(); // No objects to compare, return an empty job handle
 			}
 
-			return new CollisionsJob()
+			// Run another job if we are evaluation collisions with ourself.
+			// This job check so we are not detecting a collision between the same entity
+			// with the price of a extra compare.
+			if (this == other)
+			{
+				return new CollisionsJobVsSelf()
+				{
+					a_positions = _objectPositionsArray,
+					b_positions = other._objectPositionsArray,
+					b_positions_length = other._active,
+
+					colliderDistance = (_objectHalfSize + other._objectHalfSize) * (_objectHalfSize + other._objectHalfSize),
+					collisions = collisions,
+				}.Schedule(_active, 32);
+			}
+
+			return new CollisionsJobVsOther()
 			{
 				a_positions = _objectPositionsArray,
 				b_positions = other._objectPositionsArray,
@@ -24,12 +40,11 @@ namespace mygame
 
 				colliderDistance = (_objectHalfSize + other._objectHalfSize) * (_objectHalfSize + other._objectHalfSize),
 				collisions = collisions,
-				ignoreSameIndex = this == other // Avoid self-collision if comparing with itself
 			}.Schedule(_active, 32);
 		}
 
 		[BurstCompile]
-		struct CollisionsJob : IJobParallelFor
+		struct CollisionsJobVsSelf : IJobParallelFor
 		{
 			[ReadOnly] public NativeArray<float2> a_positions;
 			[ReadOnly] public NativeArray<float2> b_positions;
@@ -37,7 +52,6 @@ namespace mygame
 
 			public float colliderDistance;
 			public NativeArray<int> collisions;
-			public bool ignoreSameIndex;
 
 			[BurstCompile]
 			public void Execute(int index)
@@ -46,9 +60,34 @@ namespace mygame
 
 				for (int i = 0; i < b_positions_length; i++)
 				{
-					if (ignoreSameIndex && i == index)
+					if (i == index)
 						continue;   //skip self-collision
 
+					if (math.distancesq(a_pos, b_positions[i]) < colliderDistance)
+					{
+						collisions[index] = i + 1; // Store the index+1 of the collision. +1 to differentiate from no collision (0)
+					}
+				}
+			}
+		}
+
+		[BurstCompile]
+		struct CollisionsJobVsOther : IJobParallelFor
+		{
+			[ReadOnly] public NativeArray<float2> a_positions;
+			[ReadOnly] public NativeArray<float2> b_positions;
+			public int b_positions_length;
+
+			public float colliderDistance;
+			public NativeArray<int> collisions;
+
+			[BurstCompile]
+			public void Execute(int index)
+			{
+				var a_pos = a_positions[index];
+
+				for (int i = 0; i < b_positions_length; i++)
+				{
 					if (math.distancesq(a_pos, b_positions[i]) < colliderDistance)
 					{
 						collisions[index] = i + 1; // Store the index+1 of the collision. +1 to differentiate from no collision (0)
