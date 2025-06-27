0062ae08d3f67082472f304a59edb845d280b04d
Simon Rodriguez <mrsimonrodriguez@gmail.com>
2025-06-27 09:44:57 +0200

    game music manager that lowers the volume if we are waiting for spawn

---
+++ Assets/Game/Scripts/Managers/GameMusicManager.cs
@@ -0,0 +1,52 @@
+using Ami.BroAudio;
+using UnityEngine;
+using UnityEventsCenter;
+
+namespace mygame
+{
+	public class GameMusicManager : MonoBehaviour
+	{
+		const float VolumeFull = 1f;
+
+		[Header("Track")]
+		[SerializeField] SoundID _music;
+
+		[Header("Settings")]
+		[SerializeField] float _volumeLower = .25f;
+		[SerializeField] float _fadeTimeInSeconds = .5f;
+
+		IAudioPlayer _instance;
+
+		void Awake()
+		{
+			//If we dont assign any music, dont play it
+			if (!_music.IsValid())
+				return;
+
+			_instance = _music.Play();
+			_instance.SetVolume(_volumeLower); //Set initial volume to lower, as we will wait for spawn and dont want the volume to go from 100% to lower% directly
+		}
+
+		void OnDestroy()
+		{
+			if (_instance != null)
+			{
+				if (_instance.IsPlaying)
+					_instance.Stop();
+				_instance = null;
+			}
+		}
+
+		void OnEnable() => EventsCenter.Subscribe<GameEvents.WaitingForSpawnEvent>(OnWaitingForSpawn);
+		void OnDisable() => EventsCenter.Unsubscribe<GameEvents.WaitingForSpawnEvent>(OnWaitingForSpawn);
+
+		void OnWaitingForSpawn(GameEvents.WaitingForSpawnEvent ev)
+		{
+			if (_instance == null)
+				return;
+
+			_instance.SetVolume(ev.IsWaiting ? _volumeLower : VolumeFull, _fadeTimeInSeconds);
+		}
+	}
+}
+
